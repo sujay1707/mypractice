7fb4a96c0b3972b92947326071513c0e56fce2e4
SujayNitturkar <sujay.nitturkar1@gmail.com>
2020-08-30 19:28:12 +0530

    version1

---
+++ Add.java
@@ -0,0 +1,12 @@
+
+public class Add
+{
+
+	public static void main(String[] args) 
+	{
+		int a=10,b=10;
+		System.out.println(a+b);
+
+	}
+
+}
---
+++ Square.java
@@ -0,0 +1,12 @@
+
+public class Square {
+
+	public static void main(String[] args)
+	{
+		int len=10;
+		int bre=20;
+		System.out.println(len*bre);
+
+	}
+
+}

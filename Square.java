3cfb7a65d1299b15570752ed88c29890ac0a5925
SujayNitturkar <sujay.nitturkar1@gmail.com>
2020-08-31 11:59:56 +0530

    Initial

--- Square.java
+++ Square.java
@@ -4,10 +4,12 @@ public class Square {
 	public static void main(String[] args)
 	{
 		int len=10;
+		int wdth=12;
 		int bre=20;
 		System.out.println(len*bre);
 		System.out.println("Thanks");
 		System.out.println("Thank u");
+		
 
 	}
 
